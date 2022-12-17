276adabc4bf88d69c2168a7549022e5f3f628683
Alex <alex_smponias@web.de>
2022-12-17 10:33:50 +0100

    Fixed some code issues

--- Sources/NativeiOSCode/ExposeFunctions.swift
+++ Sources/NativeiOSCode/ExposeFunctions.swift
@@ -8,11 +8,28 @@
 import Foundation
 
 @_cdecl("NativeiOSCode_runNativeCode")
-public func NativeiOSCode_runNativeCode(input: String) {
-    NativeiOSCode.runNativeCode(input: input)
+public func NativeiOSCode_runNativeCode(input: UnsafePointer<CChar>?) {
+    guard let input else {
+        print("🚨 Input was null!")
+        return
+    }
+    
+    if let str = String(validatingUTF8: input) {
+        NativeiOSCode.runNativeCode(input: str)
+    }
+    else
+    {
+        print("🚨 Wrong input format!")
+    }
+    
 }
 
 @_cdecl("NativeiOSCode_runNativeCode_withoutParameter")
 public func NativeiOSCode_runNativeCode_withoutParameter() {
     NativeiOSCode.runNativeCode(input: "Hard coded parameter")
+}
+
+@_cdecl("swiftCodeKitConfigureMinMax")
+public func swiftCodeKitConfigureMinMax(min: Double, max: Double) {
+    NativeiOSCode.runNativeCode(input: String(min))
 }
